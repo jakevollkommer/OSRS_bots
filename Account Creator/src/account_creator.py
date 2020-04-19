02c7bc17316922e94d0730d4e264c064fe085ad0
harrison <harrison@legg.io>
2020-04-18 22:19:14 -0400

    insert created accounts to db

--- Account Creator/src/account_creator.py
+++ Account Creator/src/account_creator.py
@@ -1,24 +1,42 @@
+from ConfigParser import SafeConfigParser
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import pymysql
 import requests
 import argparse
 import random
 import time
 
 
-random_word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+
+def db_connect(config):
+    server = config.get("database", "server")
+    user = config.get("database", "user")
+    password = config.get("database", "password")
+    db = config.get("database", "db")
+    charset = config.get("database", "charset")
+
+    connection = pymysql.connect(host=server, user=user, password=password, db=db, charset=charset)
+
+    return connection
 
 
-def generate_random_emails(n):
-    response = requests.get(random_word_site)
+def insert_account(db, email, password):
+    query = "insert into accounts (email, password, tutorial_complete) values ('{}', '{}', false)".format(email, password)
+    db.cursor().execute(query)
+    db.commit()
+
+
+
+def generate_random_emails(n, config):
+    response = requests.get(config.get("random_words", "url"))
     words = random.sample([x for x in response.content.splitlines() if 3 < len(x) < 8], 2)
     emails = ["{}.{}{}@gmail.com".format(words[0], words[1], i) for i in range(n)]
 
     return emails
 
 
-def generate_random_passwords(n):
-    response = requests.get(random_word_site)
+def generate_random_passwords(n, config):
+    response = requests.get(config.get("random_words", "url"))
     passwords = random.sample([x for x in response.content.splitlines() if 8 < len(x) < 12], n)
 
     return passwords
@@ -30,11 +48,8 @@ def generate_random_birthdays(n):
     return birthdays
 
 
-def create_account(email, password, birthday):
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(executable_path='/users/harrison/Desktop/chromedriver', chrome_options=options)
+def create_account(db, email, password, birthday):
+    driver = webdriver.Chrome(executable_path='/users/harrison/Desktop/chromedriver')
     driver.get("https://secure.runescape.com/m=account-creation/g=oldscape/create_account")
     time.sleep(3)
 
@@ -65,18 +80,31 @@ def create_account(email, password, birthday):
     create = driver.find_element_by_id("create-submit")
     create.click()
 
+    time.sleep(3)
+
+    if 'WELCOME' in driver.page_source:
+        insert_account(db, email, passwords)
+    else:
+        print 'Failed to create account: {}'.format(email)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Bulk RS Account Creator")
     parser.add_argument("--num", type = int)
 
     args = parser.parse_args()
+    config = SafeConfigParser()
+    config.read("account_creator_config.ini")
 
-    emails = generate_random_emails(args.num)
-    passwords = generate_random_passwords(args.num)
+    db = db_connect(config)
+
+    emails = generate_random_emails(args.num, config)
+    passwords = generate_random_passwords(args.num, config)
     birthdays = generate_random_birthdays(args.num)
 
     target_accounts = dict(zip(emails, zip(passwords, birthdays)))
-
+    print target_accounts
+    #
     for k, v in target_accounts.iteritems():
-        create_account(k, v[0], v[1])
+        create_account(db, k, v[0], v[1])
